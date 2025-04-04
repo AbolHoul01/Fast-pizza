c736f8e97a8479b2b300461780991ab43bb8d66b
Abolfazl_Ramezani <abolhoul.dev@gmail.com>
2025-04-04 17:11:45 +0330

    Handling error with error element and put type guard for handle unknown data and show the error messages

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -1,15 +1,25 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function NotFound() {
+// Type guard for unknown data type
+function isErrorObject(error: unknown): error is { data?: string; message?: string } {
+  return typeof error === "object" && error !== null;
+}
+
+function Error() {
   const navigate = useNavigate();
+  const error = useRouteError();
+
+  const getErrorMessage = (error: unknown): string => {
+    return isErrorObject(error) ? error.data || error.message || "An unexpected error occurred." : "An unexpected error occurred.";
+  };
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{getErrorMessage(error)}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
