dfffa2e2d049fd46ded0f68e6e7480512c351cc8
Abolfazl_Ramezani <abolhoul.dev@gmail.com>
2025-04-04 21:43:48 +0330

    Writing data with React router actions

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { ActionFunctionArgs, Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 // const isValidPhone = (str: string): boolean =>
@@ -6,47 +8,48 @@ import { useState } from "react";
 //     str
 //   );
 
-// interface CartItem {
-//   pizzaId: number;
-//   name: string;
-//   quantity: number;
-//   unitPrice: number;
-//   totalPrice: number;
-// }
+interface CartItem {
+  pizzaId: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
+}
 
-// const fakeCart: CartItem[] = [
-//   {
-//     pizzaId: 12,
-//     name: "Mediterranean",
-//     quantity: 2,
-//     unitPrice: 16,
-//     totalPrice: 32,
-//   },
-//   {
-//     pizzaId: 6,
-//     name: "Vegetale",
-//     quantity: 1,
-//     unitPrice: 13,
-//     totalPrice: 13,
-//   },
-//   {
-//     pizzaId: 11,
-//     name: "Spinach and Mushroom",
-//     quantity: 1,
-//     unitPrice: 15,
-//     totalPrice: 15,
-//   },
-// ];
+const fakeCart: CartItem[] = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState<boolean>(false);
-  // const cart: CartItem[] = fakeCart;
+  const cart: CartItem[] = fakeCart;
 
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      {/* <Form method="POST" action="/order/new"> */}
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -78,11 +81,28 @@ function CreateOrder() {
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button type="submit">Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  const order = {
+    ...data,
+    cart: data.cart ? JSON.parse(data.cart) : [],
+    priority: Boolean(data.priority),
+  };
+
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
