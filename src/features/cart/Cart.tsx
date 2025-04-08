b66d3e8f708da7eaec735b8a969036f9b66e3ada
Abolfazl_Ramezani <abolhoul.dev@gmail.com>
2025-04-08 18:15:25 +0330

    Styling the cart

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,52 +1,59 @@
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
-// interface CartItem {
-//   pizzaId: number;
-//   name: string;
-//   quantity: number;
-//   unitPrice: number;
-//   totalPrice: number;
-// }
+interface CartItem {
+  pizzaId: number;
+  name: string;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
+}
 
-// const fakeCart: CartItem[] = [
-//   {
-//     pizzaId: 12,
-//     name: "Mediterranean",
-//     quantity: 2,
-//     unitPrice: 16,
-//     totalPrice: 32,
-//   },
-//   {
-//     pizzaId: 6,
-//     name: "Vegetale",
-//     quantity: 1,
-//     unitPrice: 13,
-//     totalPrice: 13,
-//   },
-//   {
-//     pizzaId: 11,
-//     name: "Spinach and Mushroom",
-//     quantity: 1,
-//     unitPrice: 15,
-//     totalPrice: 15,
-//   },
-// ];
+const fakeCart: CartItem[] = [
+  {
+    pizzaId: 12,
+    name: "Mediterranean",
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+  {
+    pizzaId: 6,
+    name: "Vegetale",
+    quantity: 1,
+    unitPrice: 13,
+    totalPrice: 13,
+  },
+  {
+    pizzaId: 11,
+    name: "Spinach and Mushroom",
+    quantity: 1,
+    unitPrice: 15,
+    totalPrice: 15,
+  },
+];
 
 function Cart() {
-  // const cart: CartItem[] = fakeCart;
+  const cart: CartItem[] = fakeCart;
 
   return (
-    <div>
+    <div className="py-3 px-4 ">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="mt-3 divide-y divide-stone-400 border-b border-stone-400">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.name} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" disabled={false} type="primary">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
