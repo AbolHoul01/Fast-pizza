1148a2186bcf2c7bcc80939a27c2d415f1bde6a0
Abolfazl_Ramezani <abolhoul.dev@gmail.com>
2025-04-08 19:21:56 +0330

    styling the order page

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -57,32 +57,37 @@ function CreateOrder() {
   const cart: CartItem[] = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6 ">
+      <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
+          {formErrors?.phone && <p className="text-xs mt-2 text-red-500 bg-red-200 p-2 rounded-md sm:flex-row">{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input className="input" type="text" name="address" required />
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              className="input w-full"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex gap-5 items-center">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-0"
             type="checkbox"
@@ -91,14 +96,14 @@ function CreateOrder() {
             checked={withPriority}
             onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">Want to give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type='primary'>
+          <Button disabled={isSubmitting} type="primary">
             {isSubmitting ? "Placing order..." : "Order now"}
-          </Button> 
+          </Button>
         </div>
       </Form>
     </div>
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -9,13 +9,13 @@ interface ButtonProps {
 
 function Button({ children, disabled, to, type }: ButtonProps) {
   const bace =
-    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-yellow-500 disabled:cursor-not-allowed";
+    "bg-yellow-400 text-sm uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-yellow-500 disabled:cursor-not-allowed";
 
   const styles = {
     primary: bace + " py-3 px-4 md:px-6 md:py-4",
     small: bace + " py-2 px-4 text-xs md:px-5 md:py-2.5",
     secondary:
-      "uppercase font-semibold text-stone-400 inline-block border-2 border-stone-300 tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:bg-stone-300 hover:text-stone-800 focus:ring-offset-2 focus:text-stone-800 active:bg-stone-500 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
+      "uppercase text-sm font-semibold text-stone-400 inline-block border-2 border-stone-300 tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:bg-stone-300 hover:text-stone-800 focus:ring-offset-2 focus:text-stone-800 active:bg-stone-500 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
   };
 
   if (to)
