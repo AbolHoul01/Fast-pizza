9cb2f976e2564dd8b43cc0918eee9226590b1d04
Abolfazl_Ramezani <abolhoul.dev@gmail.com>
2025-04-09 17:25:09 +0330

    Modeling the user state with RTK

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,42 +1,62 @@
-interface Position {
-  latitude: number;
-  longitude: number;
-}
+// interface Position {
+//   latitude: number;
+//   longitude: number;
+// }
 
-interface Address {
-  locality?: string;
-  city?: string;
-  postcode?: string;
-  countryName?: string;
-}
+import { createSlice } from "@reduxjs/toolkit";
 
-function getPosition(): Promise<GeolocationPosition> {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-}
+// interface Address {
+//   locality?: string;
+//   city?: string;
+//   postcode?: string;
+//   countryName?: string;
+// }
 
-async function getAddress(position: Position): Promise<Address> {
-  const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.latitude}&longitude=${position.longitude}`
-  );
-  if (!res.ok) throw new Error("Failed to fetch address");
-  
-  return res.json();
-}
+// function getPosition(): Promise<GeolocationPosition> {
+//   return new Promise((resolve, reject) => {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   });
+// }
 
-async function fetchAddress(): Promise<{ position: Position; address: string }> {
-  // 1) We get the user's geolocation position
-  const positionObj = await getPosition();
-  const position: Position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
+// async function getAddress(position: Position): Promise<Address> {
+//   const res = await fetch(
+//     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.latitude}&longitude=${position.longitude}`
+//   );
+//   if (!res.ok) throw new Error("Failed to fetch address");
 
-  // 2) Then we use a reverse geocoding API to get a description of the user's address
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality || ''}, ${addressObj?.city || ''} ${addressObj?.postcode || ''}, ${addressObj?.countryName || ''}`.trim();
+//   return res.json();
+// }
 
-  // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-}
+// async function fetchAddress(): Promise<{ position: Position; address: string }> {
+//   // 1) We get the user's geolocation position
+//   const positionObj = await getPosition();
+//   const position: Position = {
+//     latitude: positionObj.coords.latitude,
+//     longitude: positionObj.coords.longitude,
+//   };
+
+//   // 2) Then we use a reverse geocoding API to get a description of the user's address
+//   const addressObj = await getAddress(position);
+//   const address = `${addressObj?.locality || ''}, ${addressObj?.city || ''} ${addressObj?.postcode || ''}, ${addressObj?.countryName || ''}`.trim();
+
+//   // 3) Then we return an object with the data that we are interested in
+//   return { position, address };
+// }
+
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload;
+    },
+  },
+});
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer
